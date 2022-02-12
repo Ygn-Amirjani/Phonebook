48550253b8a6e742757660f7f86a695ffd95fb25
Yegane <yeganeh.a.m7979@gmail.com>
2022-02-12 14:06:15 +0330

    Python connection to the database.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, json
+from config import Config
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route('/')
-def check():
-    return json.dumps({"Hello": "World"})
+def index():
+    return jsonify("Just For Check :) ")
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
