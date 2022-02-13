51394e3fd87047880ec7165f4260f4a0ef604a5c
Yegane <yeganeh.a.m7979@gmail.com>
2022-02-13 12:02:42 +0330

    Just connect to the database and create a table app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from conf.config import Config
+from flask import Flask, jsonify
+from flask_restful import Api
+from src.models.user import User
+from db import db
+
+# Construct the core application.
+app = Flask(__name__)
+api = Api(app)
+
+# configuration
+app.debug = True
+app.config.from_object(Config)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return jsonify("Just For Check :) ")
+
+
+@app.before_first_request
+def create_tables():
+    """ To create/use the database mentioned in the URI, run this function. """ 
+    User.__table__
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run()
